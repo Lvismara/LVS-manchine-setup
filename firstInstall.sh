7ece56c6c43754bb69a87af2039bb3a345f7e8c5
Lvismara <leo.vismara@gmail.com>
2018-06-28 08:30:41 -0300

    rm google

--- firstInstall.sh
+++ firstInstall.sh
@@ -12,29 +12,56 @@ echo ""
 echo ""
 echo ""
 sudo apt-get update
+echo
 echo "..."
+echo
 echo "..."
+echo
 sudo apt-get install software-properties-common
+echo
 echo "..."
+echo
 echo "..."
+echo
 sudo apt-add-repository ppa:ansible/ansible
+echo
 echo "..."
+echo
 echo "..."
+echo
 sudo apt-get update
+echo
 echo "..."
+echo
 echo "..."
-sudo apt-get install ansible
+echo
+sudo apt-get install -y ansible
+echo
 echo "..."
+echo
 echo "..."
+echo
 
 echo "//////////////////////////// Done //////////////////////////// "
 
+echo
+echo "..."
 echo ""
+echo "..."
 echo ""
+echo "..."
 echo ""
 echo "verify the ansible varsion ..."
 echo ""
 echo "ansible --version"
+echo "..."
+echo ""
+echo "..."
+echo ""
+echo "..."
 echo "Start ansible setup ..."
 echo ""
 echo "Run: ansible-playbook -i hosts setup.yml for install all the setup ..."
+echo "..."
+echo ""
+echo "//////////////////////////// Finish ////////////////////////////"
